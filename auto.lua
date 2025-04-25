38ba7077e9c861bf0766e7d56ef601abda860f4f
eruiier <eeeiqjjj200@gmail.com>
2025-04-24 23:30:13 -0400

    Update auto.lua

--- auto.lua
+++ auto.lua
@@ -1,5 +1,20 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
 
+local player = Players.LocalPlayer
+
+local function noclip()
+    if player.Character then
+        for _, part in pairs(player.Character:GetDescendants()) do
+            if part:IsA("BasePart") then
+                part.CanCollide = false
+            end
+        end
+    end
+end
+
+RunService.Stepped:Connect(noclip)
+
+
 local teleportTarget = Vector3.new(-332.19, 3, -49047.07)
 
 task.wait(5) -- Wait for 5 seconds
