9857cf1c892d2f3bf5a66702f33e25c14989d3d9
eruiier <eeeiqjjj200@gmail.com>
2025-04-24 23:47:10 -0400

    Update auto.lua

--- auto.lua
+++ auto.lua
@@ -24,9 +24,21 @@ end
 -- Connect to RunService to continuously apply noclip
 RunService.Stepped:Connect(noclip)
 
+task.spawn(function()
+    wait(20) -- Wait 20 seconds before starting
+    
+    while true do
+        local args = {
+            [1] = false
+        }
 
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("EndDecision"):FireServer(unpack(args))
+        
+        wait(3) -- Wait 3 seconds before repeating
+    end
+end)
 
-local teleportTarget = Vector3.new(-332.19, 3, -49047.07)
+local teleportTarget = Vector3.new(147.79, 5.77, 29981.89)
 
 task.wait(5) -- Wait for 5 seconds
 game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(teleportTarget))
