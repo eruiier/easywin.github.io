b83f5ac5f70db21c691640c12d4aba7a7fbddb7a
eruiier <eeeiqjjj200@gmail.com>
2025-04-24 23:29:25 -0400

    Update auto.lua

--- auto.lua
+++ auto.lua
@@ -1,1 +1,6 @@
-e
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
+
+local teleportTarget = Vector3.new(-332.19, 3, -49047.07)
+
+task.wait(5) -- Wait for 5 seconds
+game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(teleportTarget))
