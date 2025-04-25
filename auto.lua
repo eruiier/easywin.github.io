8b4befddc74700c4a5efb6fe73478c1d08890c03
eruiier <eeeiqjjj200@gmail.com>
2025-04-24 23:36:59 -0400

    Update auto.lua

--- auto.lua
+++ auto.lua
@@ -1,7 +1,16 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
 
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 local player = Players.LocalPlayer
 
+-- Ensure the player object exists
+while not player do
+    task.wait()
+    player = Players.LocalPlayer
+end
+
+-- Function to enable noclip
 local function noclip()
     if player.Character then
         for _, part in pairs(player.Character:GetDescendants()) do
@@ -12,7 +21,9 @@ local function noclip()
     end
 end
 
+-- Connect to RunService to continuously apply noclip
 RunService.Stepped:Connect(noclip)
+
 
 
 local teleportTarget = Vector3.new(-332.19, 3, -49047.07)
